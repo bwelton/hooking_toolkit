19c3e0143c7af0bf4fc36f0d7219a0af124d4940
Benjamin Welton <welton@cs.wisc.edu>
2017-07-13 17:56:15 -0400

    Fixed issue with simple hooks typing

--- src/stacktrace/SimpleHooks.cpp
+++ src/stacktrace/SimpleHooks.cpp
@@ -6,7 +6,7 @@ extern "C" {
 void LIBRARY_HOOK_LOCATION() {
     //fprintf(stderr, "I SHOULD NOT BE HERE\n");
 }
-void TRACK_COPIES(int identifier, int64 transfer) {
+void TRACK_COPIES(int identifier, int64_t transfer) {
 
 }
 
